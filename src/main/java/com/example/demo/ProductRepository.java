5e9fece806110ef1ead4bcc30f800525ad75b9d7
ishimagi <91235066+ishimagi@users.noreply.github.com>
2022-04-15 22:00:44 +0530

    Delete src directory

--- src/main/java/com/example/demo/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo;
-
-import com.example.demo.Bean.Product;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepository extends CrudRepository<Product,Integer> {
-}
